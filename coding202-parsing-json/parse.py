abd16be2bdba0d2db992caf9b5a021686e169d97
Mike Maas <mikemaas@cisco.com>
2015-06-09 11:40:05 -0700

    Adding parsing workshop skeleton file.

---
+++ coding202-parsing-json/parse.py
@@ -0,0 +1,31 @@
+# 0 Type anything (Temp)
+
+# 1 Import our required libraries: Request, urlopen, json
+
+# 2 Specify our URI
+
+# 3 Setup a connection Request
+
+# 4 Add the appropriate headers to our HTTP request
+
+# 5 Do the actual request and collect the response with urlopen (Also do # 15)
+
+# 6 Store the Response string
+
+# 7 Load the JSON Object
+
+# 8 Print the JSON Object (Temp)
+
+# 9 Extract the floors from Building\Floor
+
+# 10 Loop through the Floors
+
+# 11 Print the Floor (Temp)
+
+# 12 Get the APs from the Floor
+
+# 13 Print the APs
+
+# 14 Loop through the APs printing them out
+
+# 15 Close the response
