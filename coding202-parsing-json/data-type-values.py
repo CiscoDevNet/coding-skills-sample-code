82398037f751b5d9cd53e8f5b873cce038ad2c36
brtiller <brtiller@cisco.com>
2016-05-06 17:59:10 -0400

    changes

--- coding202-parsing-json/data-type-values.py
+++ coding202-parsing-json/data-type-values.py
@@ -1,9 +1,9 @@
-my_list=[5,10,'joe']
-print (my_list[2])
+my_list=[5,'joe']
+print (my_list[1])
 
 my_tuple=('brett','Cisco',9)
 print (my_tuple[1])
 
 
-my_dict={"red":1,"green":2,"blue":3}
+my_dict={"red":1,"green":2}
 print(my_dict["green"])
