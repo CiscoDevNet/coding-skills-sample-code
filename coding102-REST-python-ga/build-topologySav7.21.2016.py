eb8b4158c333551ab751950ed3cc8c4d7c6e127c
brtiller <brtiller@cisco.com>
2016-07-21 18:16:59 -0400

    build topology fixes

--- coding102-REST-python-ga/build-topologySav7.21.2016.py
+++ coding102-REST-python-ga/build-topologySav7.21.2016.py
@@ -77,50 +77,43 @@ def getTopology(ticket):
 	r_json=response.json()
 	
 	#Iterate through network device data and list the nodes, their interfaces, status and to what they connect	
-	for n in r_json["response"]["nodes"]:		
-		print()
-		print()
-		print('{:30}'.format("Node") + '{:25}'.format("Family") + '{:20}'.format("Label")+ "Management IP")
+	for n in r_json["response"]["nodes"]:
 		if "platformId" in n:
-			print('{:30}'.format(n["platformId"]) + '{:25}'.format(n["family"]) + '{:20.14}'.format(n["label"]) + n["ip"])
-		else:
-			print('{:30}'.format(n["role"]) + '{:25}'.format(n["family"]) + '{:20.14}'.format(n["label"]) + n["ip"])
+			print()
+			print()
+			print('{:30}'.format("Node") + '{:25}'.format("Family") + '{:20}'.format("Label")+ "Management IP")
+			print('{:30}'.format(n["platformId"]) + '{:25}'.format(n["family"]) + '{:20}'.format(n["label"]) + n["ip"])		
 		found=0    #print header flag
 		printed=0  #formatting flag
 		for i in r_json["response"]["links"]:
-			#if "startPortName" in i:
+			if "startPortName" in i:
 				#check that the source device id for the interface matches the node id.  Means interface originated from this device. 
-			if i["source"] == n["id"]:
-				if found==0:
-					print('{:>20}'.format("Source Interface") + '{:>15}'.format("Target") +'{:>28}'.format("Target Interface") + '{:>15}'.format("Status") )
-					found=1
-					printed=1					
-				for n1 in r_json["response"]["nodes"]:
-					#find name of node to which this one connects
-					if i["target"] == n1["id"]:
-						if "startPortName" in i:
-							print("    " + '{:<25}'.format(i["startPortName"]) + '{:<18.14}'.format(n1["label"]) + '{:<25}'.format(i["endPortName"]) + '{:<9}'.format(i["linkStatus"]) )
-						else:
-							print("    " + '{:<25}'.format("unknown") + '{:<18.14}'.format(n1["label"]) + '{:<25}'.format("unknown") + '{:<9}'.format(i["linkStatus"]) )
-						break;
-		found=0				
+				if i["source"] == n["id"]:
+					if found==0:
+						print('{:>20}'.format("Source Interface") + '{:>15}'.format("Target") +'{:>28}'.format("Target Interface") + '{:>15}'.format("Status") )
+						found=1
+						printed=1					
+					for n1 in r_json["response"]["nodes"]:
+						#find name of node to which this one connects
+						if i["target"] == n1["id"]:
+							print("    " + '{:<25}'.format(i["startPortName"]) + '{:<18}'.format(n1["platformId"]) + '{:<25}'.format(i["endPortName"]) + '{:<9}'.format(i["linkStatus"]) )							
+							break;
+		found=0		
+		
 		for i in r_json["response"]["links"]:
-			#if "startPortName" in i:
-			#Find interfaces that link to this one which means this node is the target. 
-			if i["target"] == n["id"]:
-				if found==0:
-					if printed==1:
-						print()
-					print('{:>10}'.format("Source") + '{:>30}'.format("Source Interface") + '{:>25}'.format("Target Interface") + '{:>13}'.format("Status"))
-					found=1					
-				for n1 in r_json["response"]["nodes"]:
-					#find name of node to that connects to this one
-					if i["source"] == n1["id"]:
-						if "startPortName" in i:							
-							print("    " + '{:<20}'.format(n1["label"]) + '{:<25}'.format(i["startPortName"]) + '{:<23}'.format(i["endPortName"]) + '{:<8}'.format(i["linkStatus"]))
-						else:
-							print("    " + '{:<20}'.format(n1["label"]) + '{:<25}'.format("unknown") + '{:<23}'.format("unknown") + '{:<8}'.format(i["linkStatus"]))
-						break;
+			if "startPortName" in i:
+				#Find interfaces that link to this one which means this node is the target. 
+				if i["target"] == n["id"]:
+					if found==0:
+						if printed==1:
+							print()
+						print('{:>10}'.format("Source") + '{:>30}'.format("Source Interface") + '{:>25}'.format("Target Interface") + '{:>13}'.format("Status"))
+						found=1					
+					for n1 in r_json["response"]["nodes"]:
+						#find name of node to that connects to this one
+						if i["source"] == n1["id"]:							
+							print("    " + '{:<20}'.format(n1["platformId"]) + '{:<25}'.format(i["startPortName"]) + '{:<23}'.format(i["endPortName"]) + '{:<8}'.format(i["linkStatus"]))
+							break;
 		
 theTicket=getTicket()
 getTopology(theTicket)
